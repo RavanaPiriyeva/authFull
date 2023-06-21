8b547743d5020da472847618e6d6dfe6bc08afa2
RavanaPiriyeva <7j59c43@code.edu.az>
2023-06-21 17:14:09 +0400

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,37 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { Chat } from './pages/Chat';
-import { Layout } from './components/Layout';
-import { RegisterPage } from './pages/Auth/Register';
-import { LoginPage } from './pages/Auth/Login';
-import Confirm from './pages/Auth/Confirm';
-import { useSelector } from 'react-redux';
-
-
+import { Chat } from "./pages/Chat";
+import { Layout } from "./components/Layout";
+import { RegisterPage } from "./pages/Auth/Register";
+import { LoginPage } from "./pages/Auth/Login";
+import Confirm from "./pages/Auth/Confirm";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getToken } from "./store/userSlice";
 
 function App() {
-  const { email, loading, error, token, succes } = useSelector(state => state.userReducer);
+  const { email, loading, error, token, succes } = useSelector(
+    (state) => state.userReducer
+  );
+  let dispatch = useDispatch();
 
+  useEffect(() => {
+    const storedItem = localStorage.getItem("token");
+    if (storedItem) {
+      const tokenObj = {
+        token: storedItem,
+      };
+      dispatch(getToken(tokenObj));
+    }
+  }, []);
   return (
     <Routes>
-      <Route path="/confirm" element={<Confirm />} />
-
       <Route path="/" element={succes.message ? <Layout /> : <RegisterPage />}>
         <Route index element={<Chat />} />
         <Route path="/signin" element={<LoginPage />} />
         {/* <Route path="/signup" element={<RegisterPage />} /> */}
-
       </Route>
+      <Route path="/confirm" element={<Confirm />} />
     </Routes>
   );
 }
